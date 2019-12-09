59dcada1e2c6d1bff360f8a9df6fab9c7f2a79de
Ryan Kubik <rkubik@twilio.com>
2019-12-08 16:37:50 -0800

    Update childhood

--- src/components/chests/childhoodGames.js
+++ src/components/chests/childhoodGames.js
@@ -3,10 +3,47 @@ import React from "react";
 const Childhood = () => (
   <>
     <h1>Childhood Games</h1>
-    <p>I played a lot of games where you could make stuff</p>
-    <p>Zoo Tycoon</p>
-    <p>Age of Empires</p>
-    <p>Halo 3</p>
+    <p>I played a lot of games as a kid where you could make stuff.</p>
+    <ul>
+      <li>
+        <a
+          href="https://www.google.com/search?q=original+zoo+tycoon&tbm=isch"
+          target="_blank"
+        >
+          Zoo Tycoon
+        </a>
+      </li>
+      <li>
+        {" "}
+        <a
+          href="https://www.google.com/search?q=age+of+empires&tbm=isch"
+          target="_blank"
+        >
+          Age of Empires
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.google.com/search?tbm=isch&sa=1&ei=l5XtXduSF4vu-gTt4YN4&q=halo+3+%22forge%22&oq=halo+3+%22forge%22&gs_l=img.3..0l4j0i30j0i8i30j0i24l4.23033.24019..24227...0.0..0.103.322.3j1......0....1..gws-wiz-img.......0i67j0i7i30.zubdqngD4jo&ved=0ahUKEwibutT-p6fmAhULt54KHe3wAA8Q4dUDCAc&uact=5#imgrc=iKO_XF4Sjp7mcM:"
+          target="_blank"
+        >
+          Halo 3
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.google.com/search?biw=1440&bih=798&tbm=isch&sa=1&ei=75XtXdbXEITB-gTKzrXIBA&q=minecraft+mcedit&oq=minecraft+mcedit&gs_l=img.3..0j0i24l9.1132.2310..2416...0.0..0.79.479.7......0....1..gws-wiz-img.......0i67j0i8i30j0i10i24.bMweE_gLVhc&ved=0ahUKEwiWi8moqKfmAhWEoJ4KHUpnDUkQ4dUDCAc&uact=5"
+          target="_blank"
+        >
+          Minecraft
+        </a>
+      </li>
+    </ul>
+    <p>
+      I spent more time thinking about new things to create and drawing them out
+      during math class than paying attention. I am still quite bad at my times
+      table.
+    </p>
   </>
 );
 
